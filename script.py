c2bfd4f2359e54ce013c2c719542672998e4d340
Myriam ORAL <myriam.oral38@gmail.com>
2024-02-29 16:06:07 +0100

    Ajout script Planets

--- script.py
+++ script.py
@@ -1,1 +1,29 @@
-s
+import requests
+import pandas as pd
+
+def get_swapi_data(resource):
+    url = f"https://swapi.dev/api/{resource}/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data from {url}")
+        return None
+
+def get_planets():
+    planets_data = get_swapi_data("planets")
+    if planets_data:
+        return planets_data.get("results", [])
+    else:
+        return []
+
+def create_dataframe_from_swapi(resource):
+    data = get_swapi_data(resource)
+    if data:
+        return pd.DataFrame(data.get("results", []))
+    else:
+        return pd.DataFrame()
+
+if __name__ == "__main__":
+    planets_df = create_dataframe_from_swapi("planets")
+    print(planets_df.head())
